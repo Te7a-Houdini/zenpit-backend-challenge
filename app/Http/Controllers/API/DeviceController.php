0611ddad4c5eb18c132c76c689eb05e0ffad498c
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-07-04 22:45:12 +0200

    Add creating device

--- app/Http/Controllers/API/DeviceController.php
+++ app/Http/Controllers/API/DeviceController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\DevicesResource;
 use App\Device;
+use App\Http\Requests\DeviceRequest;
 
 class DeviceController extends Controller
 {
@@ -25,9 +26,11 @@ class DeviceController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(DeviceRequest $request)
     {
-        //
+        return new DevicesResource(
+            Device::create($request->validated())
+        );
     }
 
     /**
