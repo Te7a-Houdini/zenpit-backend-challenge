bfaa7c458c311907fcd3801e43d86f8a7711fe54
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-07-04 23:47:00 +0200

    Add testing update device

--- tests/Feature/DevicesTest.php
+++ tests/Feature/DevicesTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Device;
 
 class DevicesTest extends TestCase
 {
@@ -20,6 +21,24 @@ class DevicesTest extends TestCase
         ->assertStatus(201)
         ->assertJson([
             'data' => ['name' => 'created test device'],
+        ]);
+    }
+
+    public function test_update_device()
+    {
+        $device = factory(Device::class)->create();
+ 
+        $this
+        ->json(
+            'PUT',
+            '/api/devices/'.$device->id,
+            array_merge($device->toArray(), [
+                'name' => 'updated device name'
+            ])
+        )
+        ->assertStatus(200)
+        ->assertJson([
+            'data' => ['name' => 'updated device name'],
         ]);
     }
 }
